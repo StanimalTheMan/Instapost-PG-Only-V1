d61b29b9dafeb47a08a3d81017d397cbf7ca2829
Stan Choi <sjc591@nyu.edu>
2024-01-15 06:30:38 -0500

    Add text field and optional title field

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    title = models.CharField(max_length=255, null=True, blank=True)
+    text = models.TextField()
     image = models.ImageField(upload_to='photos/')
     timestamp = models.DateTimeField(auto_now_add=True)
     moderation_status = models.CharField(max_length=20, default='pending')
