434318a12c746b1de6c59ae7778a12d983066562
Stan Choi <sjc591@nyu.edu>
2024-01-15 06:22:48 -0500

    Change project name

--- instapost_pgonly_v1/asgi.py
+++ instapost_pgonly_v1/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for instagram_pgonly_v1 project.
+ASGI config for instapost_pgonly_v1 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instagram_pgonly_v1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instapost_pgonly_v1.settings')
 
 application = get_asgi_application()
--- instapost_pgonly_v1/urls.py
+++ instapost_pgonly_v1/urls.py
@@ -1,4 +1,4 @@
-"""instagram_pgonly_v1 URL Configuration
+"""instapost_pgonly_v1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
